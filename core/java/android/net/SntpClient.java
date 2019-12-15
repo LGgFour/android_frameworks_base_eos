59b31a54c085e72347775c8a0c889b739c9d5307
frank PREEL <frank.preel@digitalairways.com>
2019-12-15 13:38:06 +0100

    Remove debug info

--- core/java/android/net/SntpClient.java
+++ core/java/android/net/SntpClient.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2019 e.foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +39,7 @@ import java.util.Arrays;
  */
 public class SntpClient {
     private static final String TAG = "SntpClient";
-    private static final boolean DBG = true;
+    private static final boolean DBG = false;
 
     private static final int REFERENCE_TIME_OFFSET = 16;
     private static final int ORIGINATE_TIME_OFFSET = 24;
