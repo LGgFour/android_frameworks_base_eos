b0078d862e91d64a1a5504c656164ecea10da8ba
electimon <electimon@gmail.com>
2023-08-19 22:31:20 +0800

    treewide: Extend PowerExt's influence over critical areas
    * Introduces implementations for
      FLING_BOOST, ACTIVITY_SWITCH, APP_ROTATE,
      PACKAGE_SWITCH, PROCESS_CREATE and PACKAGE_INSTALL.
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: Ib9182941f81d3a1c9a08b7c1339e865dc7906574

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -107,6 +107,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.os.PowerManagerInternal.PowerExtBoosts;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
@@ -398,6 +399,8 @@ public class DisplayPolicy {
     private final ForceShowNavBarSettingsObserver mForceShowNavBarSettingsObserver;
     private boolean mForceShowNavigationBarEnabled;
 
+    // Fling boost tracker
+    private boolean mFlingBoosting = false;
     private class PolicyHandler extends Handler {
 
         PolicyHandler(Looper looper) {
@@ -548,9 +551,10 @@ public class DisplayPolicy {
 
                     @Override
                     public void onFling(int duration) {
-                        if (mService.mPowerManagerInternal != null) {
-                            mService.mPowerManagerInternal.setPowerBoost(
-                                    Boost.INTERACTION, duration);
+                        if (mService.mPowerManagerInternal != null && !mFlingBoosting) {
+                            mService.mPowerManagerInternal.setPowerExtBoost(
+                                    PowerExtBoosts.FLING_BOOST.name(), duration);
+                            mFlingBoosting = true;
                         }
                     }
 
@@ -569,6 +573,9 @@ public class DisplayPolicy {
                         final WindowOrientationListener listener = getOrientationListener();
                         if (listener != null) {
                             listener.onTouchStart();
+                        }
+                        if (mFlingBoosting) {
+                            mFlingBoosting = false;
                         }
                     }
 
